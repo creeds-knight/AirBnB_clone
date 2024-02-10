c77a7c8f5a9b9d6e08af7e766720f5df6de9b7f3
creeds-knight <apedoarthur21@gmail.com>
2024-02-10 13:28:48 +0300

    updating file_storage.py to have private class attributes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -16,33 +16,30 @@ class FileStorage():
     __file_path = "file.json"
     __objects = {}
 
-    def __init__(self):
-        """ A method to initialise the filestorage class"""
-        self.__objects = {}
 
     def all(self):
         """ A method to return __object"""
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """ A method to set values of __objects"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        self.__objects[key] = obj
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """ A method to serialise objects to a file path"""
-        ser_obj = {key: obj.to_dict() for key, obj in self.__objects.items()}
-        with open(self.__file_path, 'w') as f:
+        ser_obj = {key: obj.to_dict() for key, obj in FileStorage.__objects.items()}
+        with open(FileStorage.__file_path, 'w') as f:
             json.dump(ser_obj, f)
 
     def reload(self):
         """ A methd to deserialise objects from a file"""
         classes = ["BaseModel", "User", "State",
                    "City", "Amenity", "Place", "Review"]
-        if not os.path.exists(self.__file_path):
+        if not os.path.exists(FileStorage.__file_path):
             pass
         else:
-            with open(self.__file_path, 'r') as f:
+            with open(FileStorage.__file_path, 'r') as f:
                 loaded_obj = json.load(f)
                 for key, value in loaded_obj.items():
                     class_name, obj_id = key.split('.')
@@ -54,4 +51,4 @@ class FileStorage():
                         module = importlib.import_module(module_name)
                         class_ = getattr(module, class_name)
                         obj_instances = class_(**value)
-                        self.__objects[key] = obj_instances
+                        FileStorage.__objects[key] = obj_instances
