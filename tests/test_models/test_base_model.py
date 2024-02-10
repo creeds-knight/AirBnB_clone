377b2d653f4ec6e35a884dcaabf325d2ee815dd9
creeds-knight <apedoarthur21@gmail.com>
2024-02-10 12:24:07 +0300

    correcting pycodestyle errors and documentations

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -9,41 +9,56 @@ import uuid
 class TestBaseModel(unittest.TestCase):
     """ A class to test each method in the BaseModel class"""
 
-
     def test_id(self):
+        """ A test case to check if ids assigned are
+            of uuid4()
+        """
         self.assertTrue(uuid.UUID(BM.id, version=4))
 
     def test_created_at(self):
+        """ A test case to check if createda_at is from
+            the datetime class
+        """
         self.assertIsInstance(BM.created_at, datetime.datetime)
 
     def test_updated_at(self):
+        """ A test case to check if updated at_at is from
+            the datetime class
+        """
         self.assertIsInstance(BM.updated_at, datetime.datetime)
 
     def test_save(self):
+        """ A test case to check if the save method changes the
+            time after saving to file
+        """
         prev_time = BM.updated_at
         BM.save(BM)
         self.assertNotEqual(prev_time, BM.updated_at)
 
     def test_str_(self):
+        """ A test case to test the output of the __str__ method"""
         bm_instance = BM()
         expected_str = f"[BaseModel] ({bm_instance.id}) {bm_instance.__dict__}"
         self.assertEqual(str(bm_instance), expected_str)
 
     def test_to_dict(self):
+        """ A test case to test the conversion to a dictionary in the
+            to_dict() method """
         expected_keys = ['id', 'created_at', 'updated_at', '__class__']
         iso_format = "%Y-%m-%dT%H:%M:%S.%f"
         for key in expected_keys:
             self.assertIn(key, BM.to_dict(BM))
         crt_str = BM.to_dict(BM)['created_at']
         upt_str = BM.to_dict(BM)['updated_at']
-        self.assertEqual(datetime.datetime.strptime(crt_str, iso_format), BM.created_at)
-        self.assertEqual(datetime.datetime.strptime(upt_str, iso_format), BM.updated_at)
+        self.assertEqual(datetime.datetime.strptime
+                         (crt_str, iso_format), BM.created_at)
+        self.assertEqual(datetime.datetime.strptime
+                         (upt_str, iso_format), BM.updated_at)
 
     def test_init_(self, *args, **kwargs):
+        """ a test case to test the initialisation of the BaseModel"""
         inst1 = BM(name="apedo", passion="alx SE")
         self.assertIn('created_at', inst1.__dict__)
         self.assertIn('updated_at', inst1.__dict__)
         self.assertIn('id', inst1.__dict__)
         self.assertIn('passion', inst1.__dict__)
-
-
--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -12,10 +12,12 @@ from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 
+
 class TestFileStorage(unittest.TestCase):
     """ A class defining all testcases for filestorage class """
 
     def setUp(self):
+        """ Initialisation of all instances for unit tests """
         self.storage = FS()
         self.file_path = "file.json"
         self.b_m = BM()
@@ -26,52 +28,60 @@ class TestFileStorage(unittest.TestCase):
         self.p_c = Place()
         self.r_v = Review()
 
-
-
     def tearDown(self):
+        """ Resetting all unittests"""
         if os.path.exists(self.file_path):
             os.remove(self.file_path)
 
     def test_all(self):
+        """ A test case to test the return value of all() method"""
         self.assertIsInstance(self.storage.all(), dict)
 
     def test_new(self):
+        """ A test case to test if a new object is created"""
         obj = BM()
         self.storage.new(obj)
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.assertIn(key, self.storage.all())
 
     def test_new_and_all(self):
+        """ A test case to check if the new object created
+            is stored in the dictionary
+        """
         self.storage.new(self.c_y)
         all_objs = self.storage.all()
         self.assertEqual(len(all_objs), 1)
         self.assertIn('City.' + self.c_y.id, all_objs)
 
     def test_save_creates_file(self):
+        """ A test case to check if the save method creates a file"""
         self.assertFalse(os.path.exists(self.file_path))
         self.storage.new(self.b_m)
         self.storage.save()
         self.assertTrue(os.path.exists(self.file_path))
 
-
     def test_file_storage_state(self):
+        """ A test case to test the file storage before items are reloaded"""
         self.assertEqual(len(self.storage.all()), 0)
 
     def test_save_and_reload(self):
+        """ A test case to test the file storage after items are reloaded"""
         self.storage.new(self.r_v)
         self.storage.save()
         self.storage.reload()
         all_objs = self.storage.all()
-        self.assertEqual(len(all_objs),1)
+        self.assertEqual(len(all_objs), 1)
         self.assertIn('Review.' + self.r_v.id, all_objs)
 
     def test_save_reload_with_no_file(self):
+        """ A test case to test reloading on non existent files"""
         self.assertFalse(os.path.exists(self.file_path))
         self.storage.reload()
         all_objs = self.storage.all()
         self.assertEqual(all_objs, {})
 
     def test_reload_with_existing_file(self):
+        """ A test case to test reloading on a file"""
         self.storage.new(self.c_y)
         self.storage.new(self.b_m)
         self.storage.new(self.a_y)
@@ -84,4 +94,3 @@ class TestFileStorage(unittest.TestCase):
         self.assertIn('BaseModel.' + self.b_m.id, all_objs)
         self.assertIn('Amenity.' + self.a_y.id, all_objs)
         self.assertIn('Review.' + self.r_v.id, all_objs)
-
